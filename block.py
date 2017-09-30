71bc7a4a507dc537ee9de36a68fc763ca36f0334
Douglas Young <douglasryoung@gmail.com>
2017-09-30 00:14:27 -0700

    Implement block equality operator

--- block.py
+++ block.py
@@ -13,3 +13,12 @@ class Block:
         self.hash = hash_fn(
             self.index, self.previous_hash, self.timestamp, self.payload, self.nonce
         )
+
+    def __eq__(self, other):
+        equal =  self.index == other.index
+        equal &= self.previous_hash == other.previous_hash
+        equal &= self.timestamp == other.timestamp
+        equal &= self.payload == other.payload
+        equal &= self.nonce == other.nonce
+        equal &= self.hash == other.hash
+        return equal
